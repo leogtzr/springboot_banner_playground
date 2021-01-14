453c8a5f69868d2bdb4f46616ac1c2b52f5d40ce
leogtzr <leogutierrezramirez@gmail.com>
2021-01-14 10:46:52 -0700

    first commit

---
+++ src/main/java/com/boot/banner/MyBannerApplication.java
@@ -0,0 +1,13 @@
+package com.boot.banner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBannerApplication {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(MyBannerApplication.class, args);
+	}
+
+}
